0113f33ee4538649be07ec644354fa5e27dc644a
odino <alessandro.nadalin@gmail.com>
2015-07-30 17:00:40 +0400

    Removing sleep dependency

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ var sleep   = require('sleep').sleep;
 function getResources(dir, config) {
   var finder    = require('findit')(dir);
   var resources = {};
-  
+
   finder.on('file', function(file, stat) {
     var url = config.baseUrl + path.relative(dir, file);
     resources[url] = 200;
   });
-  
+
   return new Promise(function(resolve){
     finder.on('end', function() {
         resolve(resources)
@@ -32,10 +32,11 @@ function smoke(resources, currentTry, config) {
     var failedResources = _.filter(report, function(resource){
       return !(resource.result);
     })
-    
+
     if (Object.keys(failedResources).length) {
-      sleep(config.sleep);
-      retry(failedResources, currentTry, config);
+      setTimeout(function(){
+        retry(failedResources, currentTry, config);
+      }, config.sleep * 1000)
     } else {
       console.log("All resources are on the CDN");
     }
@@ -46,24 +47,24 @@ function smoke(resources, currentTry, config) {
  * Gets a list of resource that weren't
  * available on the CDN and tries to smoke
  * them again.
- * 
+ *
  * This process will be repeated as many times
  * as it's defined in config.tries or --tries from
  * the command line.
- * 
+ *
  * If the resources, after the Nth try, are still
  * unavailable, the process exists with a non-zero
  * status code (exception).
  */
 function retry(failedResources, currentTry, config){
   console.log("Attempting to verify everything is on the CDN (" + currentTry + ")");
-  
+
   if (currentTry === config.tries) {
     fail(failedResources);
   } else {
     currentTry += 1;
     smoke(failedResources, currentTry, config);
-  }  
+  }
 }
 
 /**
@@ -71,20 +72,20 @@ function retry(failedResources, currentTry, config){
  */
 function fail(failedResources) {
   var message = "Some resources weren't found on the CDN";
-  
+
   _.each(failedResources, function(resource){
     message += "\n" + resource.url;
   })
-  
+
   var error = new Error(message);
   error.resources = failedResources;
-  
+
   throw error;
 }
 
 /**
  * Runs the CDN checks.
- * 
+ *
  * Be sure all resources are uploaded
  * on the CDN, else this guy is gonna
  * get mad.
@@ -98,4 +99,3 @@ function run(dir, config) {
 }
 
 module.exports = run;
-
